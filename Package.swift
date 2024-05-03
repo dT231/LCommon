83e7e1e12b725d63a7888f736d6a3f792c84b190
Danila Gorbunov <gorbunov.danila2@wb.ru>
2024-05-03 16:08:40 +0800

    add platform

--- Package.swift
+++ Package.swift
@@ -5,6 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "LCommon",
+    platforms: [
+        .iOS(
+            .v15
+        )
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
